ab92e8273b4f0156a209300f41200661dd3d547e
Everett Lachs <142778448+everettlachs@users.noreply.github.com>
2024-03-13 17:29:04 +0000

    Wed, Mar 13, 2024, 10:29 AM -07:00

---
+++ html/js/expanding.js
@@ -0,0 +1,16 @@
+panels = document.querySelectorAll('.panel')
+
+panels.forEach(panel => {
+    panel.addEventListener('click', () => {
+        remove.ActiveClass()
+        panel.classList.add('active')
+    })
+})
+
+
+
+function removeActiveClasses(){
+    panels.forEach(panel => {
+        panel.classList.remove('active')
+    })
+}
