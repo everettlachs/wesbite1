71bae47899a34a953997895730949a800ee9ffc3
Everett Lachs <142778448+everettlachs@users.noreply.github.com>
2024-03-18 00:33:10 +0000

    Sun, Mar 17, 2024, 5:33 PM -07:00

--- js/loading.js
+++ js/loading.js
@@ -1,20 +1,23 @@
-loadText = document.querySelector('.loading-text')
-bg = document.querySelector('.bg')
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
 
 
-load =
+ let load = 0;
 
 int - setInterval(blurring, 30)
 
 
 function blurring() {
     load++
+    
     if (load > 99) {
         clearInterval(int)
     }
     loadText.innerText = `${load}%`
+
+
     loadText.style.opacity = 1 - load/100;
-    bg,style.filter = `blur(${30 -(load/100)*30)}px)`
+    bg.style.filter = `blur(${30 -(load/100)*30)}px)`
 }
 
 
