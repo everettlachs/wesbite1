4f449d793cd425c24156e74ce8ad9ba9e04740bb
Everett Lachs <142778448+everettlachs@users.noreply.github.com>
2024-03-18 03:19:45 +0000

    Sun, Mar 17, 2024, 8:19 PM -07:00

---
+++ js/index.js
@@ -0,0 +1,15 @@
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    var headings = document.querySelectorAll("h1, h2, h3, h4, h5, h6");
+
+    headings.forEach(function(heading) {
+        var box = document.createElement("div");
+
+        box.classList.add("heading-box");
+
+        box.appendChild(heading.cloneNode(true));
+
+        heading.parentNode.replaceChild(box, heading);
+    });
+});
