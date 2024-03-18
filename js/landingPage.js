cd4edafe88c8af4d9a93062320c2d38c50cf3915
Everett Lachs <142778448+everettlachs@users.noreply.github.com>
2024-03-18 05:14:25 +0000

    Sun, Mar 17, 2024, 10:14 PM -07:00

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,23 +1,45 @@
-toggle = document.getElementById('toggle')
-open = document.getElementById('open')
-modal = document.getElementById('model')
-close = document.getElementById('close')
+// toggle = document.getElementById('toggle')
+// open = document.getElementById('open')
+// modal = document.getElementById('model')
+// close = document.getElementById('close')
 
+
+// toggle.addEventListener('click', () => {
+//     document.body.classList.toggle('show-nav')
+// })
+
+// open.addEventListener('click', () => {
+//     modal.classList.add('show-modal')
+// })
+
+// close.addEventListener('click', () => {
+//     modal.classList.remove('show-modal')
+// })
+
+// window.addEventListener('click', (e) => {
+//     if (e.target == modal) {
+//         modal.classList.remove
+//     }
+// })
+let toggle = document.getElementById('toggle');
+let open = document.getElementById('open');
+let modal = document.getElementById('modal');
+let close = document.getElementById('close');
 
 toggle.addEventListener('click', () => {
-    document.body.classList.toggle('show-nav')
-})
+    document.body.classList.toggle('show-nav');
+});
 
 open.addEventListener('click', () => {
-    modal.classList.add('show-modal')
-})
+    modal.classList.add('show-modal');
+});
 
 close.addEventListener('click', () => {
-    modal.classList.remove('show-modal')
-})
+    modal.classList.remove('show-modal');
+});
 
 window.addEventListener('click', (e) => {
     if (e.target == modal) {
-        modal.classList.remove
+        modal.classList.remove('show-modal');
     }
-})
+});
