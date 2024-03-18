9fc64b6c14e80ab212a5bf962289c47e65ae0c97
Everett Lachs <142778448+everettlachs@users.noreply.github.com>
2024-03-18 05:03:20 +0000

    Sun, Mar 17, 2024, 10:03 PM -07:00

--- js/landingPage.js
+++ js/landingPage.js
@@ -1,6 +1,13 @@
 toggle = document.getElementById('toggle')
+open = document.getElementById('open')
+model = document.getElementById('model')
 
 
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav')
 })
+
+open.addEventListener('click', () => {
+    modal.classList.add('show-modal')
+})
+
